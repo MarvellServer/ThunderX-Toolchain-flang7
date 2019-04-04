b503989fc6691311e00d298e7ff055964f99d28c
Stefan Teleman <stefan.teleman@cavium.com>
2019-04-04 21:28:25 +0000

    removed unnecessary files

--- git-add.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git add bindings cmake CMakeLists.txt CODE_OWNERS.TXT configure CREDITS.TXT docs examples git-add.sh include lib LICENSE.TXT LLVMBuild.txt llvm.spec.in projects README.md README.txt RELEASE_TESTERS.TXT resources runtimes test tools unittests utils
-
